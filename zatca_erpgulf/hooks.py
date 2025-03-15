bf17c703ca500d7440a44b6d4daf43448ef44911
Ahmadpasha1221 <ahmadpashashaiks@gmail.com>
2025-03-15 11:26:37 +0000

    commented wrokspace fixture for over appearing

--- zatca_erpgulf/hooks.py
+++ zatca_erpgulf/hooks.py
@@ -272,7 +272,7 @@ doctype_list_js = {
 
 # fixtures = [ {"dt": "Custom Field","filters": [["module", "=", "Zatca Erpgulf"]] }]
 fixtures = [
-    {"dt": "Workspace", "filters": {"module": "Zatca Erpgulf"}},
+    # {"dt": "Workspace", "filters": {"module": "Zatca Erpgulf"}},
     {"dt": "Custom Field", "filters": {"module": "Zatca Erpgulf"}},
     {"dt": "Report", "filters": {"module": "Zatca Erpgulf"}},
     #    {"dt": "Page", "filters": {"module": "Zatca Erpgulf"}}
